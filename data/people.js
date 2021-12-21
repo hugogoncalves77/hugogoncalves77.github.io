9aa669bc44eba3fe7815c898d199054f63f7c31c
Hugo Meireles Gonçalves <hugo.goncalves@gmail.com>
2021-12-21 22:56:19 +0000

    Add files via upload

---
+++ data/people.js
@@ -0,0 +1,183 @@
+const peopleData = {
+    "Product and Management": {
+        "Directors": {
+            "Jo&atilde;o Teixeira": "joao.teixeira.jpg",
+            "Jorge Carneiro": "jorge.carneiro.jpg"
+        },
+        "Executive Producer": {
+            "Massimiliano Spalazzi": "massimiliano.spalazzi.jpg"
+        },
+        "Product Owners": {
+            "Arthur Coquelle": "arthur.coquelle.jpg",
+            "Gaia Balbo Di Vinadio": "gaia.vinadio.jpg",
+            "Rosa Castro": "rosa.castro.jpg"
+        }
+    },
+    "Backend Development": {
+        "Development Leads": {
+            "Jo&atilde;o Ramos": "joao.ramos.jpg",
+            "Tiago Pinheiro": "tiago.pinheiro.jpg"
+        },
+        "Developers": {
+            "David Ribeiro": "david.ribeiro.jpg",
+            "H&eacute;lder Carvalho": "helder.carvalho.jpg",
+            "Hugo Gon&ccedil;alves": "hugo.goncalves.jpg",
+            "Mitchell Miranda": "mitchell.miranda.jpg",
+            "Nuno Fonseca": "nuno.fonseca.jpg",
+            "Paulo Silva": "paulo.silva.jpg",
+            "S&eacute;rgio Nogueira": "sergio.nogueira.jpg"
+        }
+    },
+    "Frontend Development": {
+        "Development Lead": {
+            "David Costa": "david.costa.jpg"
+        },
+        "Developers": {
+            "Jo&atilde;o \"Madeira\" Vieira": "joao.vieira.jpg",
+            "Nelson Silva": "nelson.silva.jpg",
+            "Pedro Teixeira": "pedro.teixeira.jpg",
+            "Vasco Leite": "vasco.leite.jpg"
+        }
+    },
+    "Mobile Development": {
+        "Development Lead": {
+            "Ricardo Dourado": "ricardo.dourado.jpg"
+        },
+        "Developers": {
+            "M&aacute;rio Costa": "mario.costa.jpg",
+            "Tiago Valente": "tiago.valente.jpg"
+        },
+        "Special Appearances": {
+            "Paulo Carvalho": "paulo.carvalho.jpg",
+            "Rui Mesquita": "rui.mesquita.jpg"
+        }
+    },
+    "User Experience": {
+        "UX Designer": {
+            "Vanessa Santos": "vanessa.santos.jpg"
+        }
+    },
+    "Quality Assurance": {
+        "QAA Manager": {
+            "Sara Alves": "sara.alves.jpg"
+        },
+        "QAA": {
+            "Elsa Leite": "elsa.leite.jpg",
+            "Jos&eacute; \"Ab&iacute;lio\" Magalh&atilde;es": "jose.magalhaes.jpg",
+            "Pedro Xavier": "pedro.xavier.jpg",
+            "Vasco Amador": "vasco.amador.jpg"
+        }
+    },
+    "DevOps and System Administration": {
+        "DevOps": {
+            "F&aacute;bio \"Toni\" Alves": "fabio.alves.jpg",
+            "Lu&iacute;s \"Xixa\" Ferreira": "luis.ferreira.jpg"
+        }
+    },
+    "Business Intelligence": {
+        "Head of Business Analytics": {
+            "Georges Bejjani": "georges.bejjani.jpg"
+        },
+        "Business Analysts": {
+            "Daniel Concei&ccedil;&atilde;o": "daniel.conceicao.jpg",
+            "Jos&eacute; Costa": "jose.costa.jpg"
+        }
+    },
+    "In Memoriam": {
+        "": {
+            "Alexandre Martins": "person.jpg",
+            "Andrea Di Natale": "andrea.dinatale.jpg",
+            "Andr&eacute; Garcia": "andre.garcia.jpg",
+            "Andreia Mota": "andreia.mota.jpg",
+            "Andreia Pacheco": "andreia.pacheco.jpg",
+            "Andr&eacute; Silva": "person.jpg",
+            "Armando Batina": "armando.batina.jpg",
+            "Artur Teixeira": "artur.teixeira.jpg",
+            "Bruno Brito": "bruno.brito.jpg",
+            "Bruno Concei&ccedil;&atilde;o": "person.jpg",
+            "Bruno Dias": "bruno.dias.jpg",
+            "Bruno Ferreira": "bruno.ferreira.jpg",
+            "Bruno Pereira": "person.jpg",
+            "Bruno Soares": "bruno.soares.jpg",
+            "Cl&eacute;ment Huon": "clement.huon.jpg",
+            "Cristina Almeida": "cristina.almeida.jpg",
+            "Cristina Teixeira": "cristina.teixeira.jpg",
+            "Damien Grandjean": "damien.grandjean.jpg",
+            "Daniela Neiva": "daniela.neiva.jpg",
+            "Danielius Isiunas": "danielius.isiunas.jpg",
+            "Daniel Loureiro": "daniel.loureiro.jpg",
+            "Daniel Sousa": "daniel.sousa.jpg",
+            "David Sainteff": "david.sainteff.jpg",
+            "Diana Santos": "diana.santos.jpg",
+            "Diogo J&uacute;nior": "diogo.junior.jpg",
+            "F&aacute;bio Mota": "fabio.mota.jpg",
+            "Gon&ccedil;alo Queir&oacute;s": "goncalo.queiros.jpg",
+            "H&eacute;lder Silva": "helder.silva.jpg",
+            "H&eacute;lder Vasconcelos": "person.jpg",
+            "Hugo Madureira": "hugo.madureira.jpg",
+            "Hugo Silva": "person.jpg",
+            "In&ecirc;s Correia": "ines.correia.jpg",
+            "Joana Ara&uacute;jo": "joana.araujo.jpg",
+            "Joana Mesquita": "joana.mesquita.jpg",
+            "Jo&atilde;o Granja": "joao.granja.jpg",
+            "Jo&atilde;o Machado": "joao.machado.jpg",
+            "Jo&atilde;o Moreira": "joao.moreira.jpg",
+            "Jo&atilde;o Pacheco": "joao.pacheco.jpg",
+            "Jo&atilde;o Pedro Pereira": "joao.pedro.pereira.jpg",
+            "Jo&atilde;o Pereira": "joao.pereira.jpg",
+            "Joaquim Silva": "joaquim.silva.jpg",
+            "Jorge de Brito": "jorge.brito.jpg",
+            "Jos&eacute; Almeida": "jose.almeida.jpg",
+            "Jos&eacute; Ferreira": "jose.ferreira.jpg",
+            "Jos&eacute; Pais": "jose.pais.jpg",
+            "Jos&eacute; Silva": "person.jpg",
+            "Julia Bruyneel": "julia.bruyneel.jpg",
+            "J&uacute;lio Rodrigues": "julio.rodrigues.jpg",
+            "Lena Ortmann": "lena.ortmann.jpg",
+            "Let&iacute;cia Azevedo": "leticia.azevedo.jpg",
+            "Luciana Silva": "luciana.silva.jpg",
+            "Lu&iacute;s Coutinho": "luis.coutinho.jpg",
+            "Manuel Silva": "person.jpg",
+            "Marcelo Teixeira": "marcelo.teixeira.jpg",
+            "Maria Jo&atilde;o Miranda": "maria.miranda.jpg",
+            "Mariana Gouveia": "mariana.gouveia.jpg",
+            "Mari-Liis Algma": "mariliis.algma.jpg",
+            "M&aacute;rio Mineiro": "mario.mineiro.jpg",
+            "Marisa Pinto": "person.jpg",
+            "Miguel Santos": "person.jpg",
+            "Nelson Canastro": "nelson.canastro.jpg",
+            "N&eacute;lson Teixeira": "nelson.teixeira.jpg",
+            "Nilay Badavne": "nilay.badavne.jpg",
+            "Nuno Castro": "nuno.castro.jpg",
+            "Nuno Lopes": "nuno.lopes.jpg",
+            "Nuno Matos": "nuno.matos.jpg",
+            "Nuno Xavier": "nuno.xavier.jpg",
+            "Paolo Fortelli": "paolo.fortelli.jpg",
+            "Pedro Gon&ccedil;alves": "pedro.goncalves.jpg",
+            "Pedro Lopes": "pedro.lopes.jpg",
+            "Pedro \"PO\" Teixeira": "pedro.teixeira.po.jpg",
+            "Rachid Mallah": "rachid.mallah.jpg",
+            "Renato Lopes": "person.jpg",
+            "Ruben Neves": "ruben.neves.jpg",
+            "Rui Almeida": "rui.almeida.jpg",
+            "Rui Costa": "rui.costa.jpg",
+            "Rui Mendes": "rui.mendes.jpg",
+            "Sami Louali": "sami.louali.jpg",
+            "Sara Vieira": "sara.vieira.jpg",
+            "Sergey Demidov": "sergey.demidov.jpg",
+            "Spela Kregar": "spela.kregar.jpg",
+            "Suzanne Pon&ccedil;on": "suzanne.poncon.jpg",
+            "Talissa Bachelot": "talissa.bachelot.jpg",
+            "T&acirc;nia Alves": "tania.alves.jpg",
+            "Tiago Dias": "tiago.dias.jpg",
+            "Tiago Moreira": "tiago.moreira.jpg",
+            "Tiago Santos": "tiago.santos.jpg",
+            "Tiago Seabra": "tiago.seabra.jpg",
+            "Tiago Silva": "tiago.silva.jpg",
+            "V&iacute;tor Rocha": "vitor.rocha.jpg",
+            "V&iacute;tor Santos": "vitor.santos.jpg",
+            "V&iacute;tor Silva": "vitor.silva.jpg",
+            "Xavier Lef&eacute;vre": "xavier.lefevre.jpg"
+        }
+    }
+};
